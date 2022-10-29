2d675de69181d0065c60f80eb1b50cfe7e95315c
ValentinoMartz <valentinomartinez55@gmail.com>
2022-10-28 23:00:00 -0300

    Text localStorage done

--- app/src/components/TextBoxBottom.jsx
+++ app/src/components/TextBoxBottom.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const TextBoxBottom = () => {
+  const [inputBottom, setInputBottom] = useState(
+    localStorage.getItem("inputBottom")
+  );
+
+  useEffect(() => {
+    JSON.stringify(localStorage.setItem("inputBottom", inputBottom));
+  }, [inputBottom]);
   let text =
     "This is a textarea. Populate it with 3 paragraphs ~ 25 words \neach. Ex: \nParagraph one (20 words)\n(line break) \nParagraph two(25 words)\n(line break)\nParagraph three (10 words)";
 
   return (
-    <div className="flex w-[50%] h-[35vh] border-b border-l border-r  shadow-[#aeacac] text-left shadow-md">
+    <div className="flex w-[50%] h-[35vh] mt-[-10%] mr-[6.3%] border-b border-l border-r  shadow-[#aeacac] text-left shadow-md">
       <div className="w-full">
         <div className="shadow-md shadow-[#f3fd83] border-b border-[#EBEFF2]">
           <p className=" pl-4 shadow-md ">Text Box</p>
@@ -13,11 +20,12 @@ const TextBoxBottom = () => {
         <div className="">
           <textarea
             type="textarea"
-            name=""
+            value={inputBottom}
             id=""
             wrap="hard"
             cols="50"
             placeholder={text}
+            onChange={(e) => setInputBottom(e.target.value)}
             className="w-full h-full outline-0 mt-2 pl-4 resize-none overflow-hidden pb-32 placeholder:text-black "
           ></textarea>
 
--- app/src/components/TextBoxTop.jsx
+++ app/src/components/TextBoxTop.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 //hacer que el texto se acomode
 //sacar la scrollbar
@@ -7,24 +9,31 @@ import React from "react";
 //max-[1000px]:ml-[2vh] max-[1200px]:ml-[350vh]
 
 const TextBoxTop = () => {
+  const [inputTop, setInputTop] = useState(localStorage.getItem("inputTop"));
+
+  useEffect(() => {
+    JSON.stringify(localStorage.setItem("inputTop", inputTop));
+  }, [inputTop]);
+
   let text =
     "This is a textarea. Populate it with 3 paragraphs ~ 25 words \neach. Ex: \nParagraph one (20 words)\n(line break) \nParagraph two(25 words)\n(line break)\nParagraph three (10 words)";
 
   return (
     <div className="flex w-[50%] h-[35vh] mt-[12.5vh]  border-b border-l border-r  shadow-[#aeacac] text-left shadow-md">
-      <div className="w-full">
+      <div className="w-full ">
         <div className="shadow-md shadow-[#f3fd83] border-b border-[#EBEFF2]">
           <p className=" pl-4 shadow-md ">Text Box</p>
         </div>
         <div className="">
           <textarea
             type="textarea"
-            name=""
+            value={inputTop}
             id=""
             wrap="hard"
             cols="50"
             placeholder={text}
-            className="w-full h-full outline-0 mt-2 pl-4 resize-none overflow-hidden pb-32 placeholder:text-black "
+            onChange={(e) => setInputTop(e.target.value)}
+            className="w-full h-full outline-0 mt-2 pl-4 resize-none overflow-hidden pb-32 placeholder:text-black"
           ></textarea>
 
           <div className=" border-t mb-[-20px] border-[#EBEFF2]"></div>
